cf9e8d8160b0a960486cf2ae68498712efaba91b
Rinchevich <daniil.ryinckevich@ukr.net>
2022-05-13 12:21:57 +0300

    Checkout in work

--- src/modal.js
+++ src/modal.js
@@ -1,31 +1,75 @@
-"use strict"
+"use strict";
 
-let buttonCountPlus = document.getElementById("buttonCountPlus");
-let buttonCountMinus = document.getElementById("buttonCountMinus");
-let count = document.getElementById("buttonCountNumber");
-let count2 = document.getElementById("num");
-let number = 1;
-let price = 200;
-let countPlus = document.querySelectorAll('.counter-plus')
-let countMinus = document.querySelectorAll('.counter-minus')
+// bag modal code
 
-buttonCountPlus.onclick = function() {
-    if (number <= 19) {
-        number++;
-        count.innerHTML = number;
-        count2.value = '$' + ' ' + number * price;
-    }
-};
+const modal = document.querySelector(".bag-modal");
+const modalOpen = document.querySelectorAll(".modal-open");
+const modalClose = document.querySelector(".modal-close");
+const wrapper = document.querySelector(".wrapp");
 
-buttonCountMinus.onclick = function() {
-   if (number >= 2) {
-       number--;
-       count.innerHTML = number;
-       count2.value = '$' + ' ' + number * price;
-    }
-}
+modalOpen.forEach((element) => {
+  element.addEventListener("click", function () {
+    modal.classList.remove("closed");
+    wrapper.classList.add("wrapper--modal");
+    document.body.style.overflow = "hidden";
+  });
+});
 
-let inputs = document.querySelectorAll('input');
+modalClose.addEventListener("click", function () {
+  modal.classList.add("closed");
+  wrapper.classList.remove("wrapper--modal");
+  document.body.style.overflow = "scroll";
+});
+
+// modal-counter
+let inputs = document.querySelectorAll("input");
 for (let index = 0; index < inputs.length; index++) {
-   inputs[index].setAttribute('readonly', 'true') 
+  inputs[index].setAttribute("readonly", "true");
 }
+
+let bagItems = document.querySelectorAll(".bag__item-wrapp");
+
+bagItems.forEach(function foo(element) {
+  const buttonCountPlus = element.querySelector(".counter-plus");
+  const buttonCountMinus = element.querySelector(".counter-minus");
+  const counterNumber = element.querySelector(".counter-number");
+  const counterValue = element.querySelector(".counter-value");
+  let price = 200;
+
+  buttonCountPlus.addEventListener("click", function () {
+    let value = parseInt(counterNumber.value);
+    if (value <= 100) {
+      value++;
+      counterNumber.value = value;
+      counterValue.value = "$" + " " + value * price;
+    }
+  });
+
+  buttonCountMinus.addEventListener("click", function () {
+    let value = parseInt(counterNumber.value);
+    if (value >= 2) {
+      value--;
+      counterNumber.value = value;
+      counterValue.value = "$" + " " + value * price;
+    }
+  });
+});
+
+const loginModal = document.querySelector('.login-modal');
+const loginOpen = document.querySelectorAll('.login-open');
+const loginClose = document.querySelector('.login-modal--close')
+
+loginOpen.forEach((element) => {
+  element.addEventListener("click", function () {
+    loginModal.classList.remove("closed");
+    wrapper.classList.add("wrapper--modal");
+    document.body.style.overflow = "hidden";
+  });
+})
+
+loginClose.addEventListener("click", function () {
+  loginModal.classList.add("closed");
+  wrapper.classList.remove("wrapper--modal");
+  document.body.style.overflow = "scroll";
+});
+
